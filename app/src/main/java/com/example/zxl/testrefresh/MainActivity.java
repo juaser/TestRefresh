3ee53991af254d1813c1fb766770ed9859410c6e
zxl <807130989@qq.com>
2016-11-11 18:36:19 +0800

    test

--- app/src/main/java/com/example/zxl/testrefresh/MainActivity.java
+++ app/src/main/java/com/example/zxl/testrefresh/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.zxl.testrefresh;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * 当 Activity 以全屏模式运行时，允许 Android 系统状态栏在顶层出现，而且不迫使 Activity 重新布局让出空间
+ */
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
         findViewById(R.id.tv_jump).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -17,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
+    }
+
+    private void hideStatusBar() {
+        WindowManager.LayoutParams attrs = getWindow().getAttributes();
+        attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setAttributes(attrs);
+    }
+
+    private void showStatusBar() {
+        WindowManager.LayoutParams attrs = getWindow().getAttributes();
+        attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setAttributes(attrs);
     }
 }
